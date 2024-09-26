6f7ebc1e1928310c21934a02794ce9a6dbc57c7c
Анастасия Вакорина <avonlyone13@gmail.com>
2024-09-26 23:06:01 +0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+def prime_factors(number):
+    a = 2
+    Factors = []
+    while a*a <= number:
+        if number % a == 0:
+            Factors.append(a)
+            number //= a
+        elif number % a != 0:
+            a += 1
+    if number > 1:
+            Factors.append(number)
+    return(Factors)
+
+number = int(input())
+A = prime_factors(number)
+
+print(*A, sep = ' * ')
